1d83af779ede899662ca89a51a8e8f29eb6dd0c9
Madeline Liao <madeline.m.liao@gmail.com>
2021-12-14 19:10:48 -0800

    add scripts

--- scripts/gen_data/gen_oracle_reach2d_data_1k.sh
+++ scripts/gen_data/gen_oracle_reach2d_data_1k.sh
@@ -2,8 +2,9 @@
 
 python run_thriftydagger.py \
         --gen_data \
-        --environment PickPlace \
+        --environment Reach2D \
+        --bc_only \
         --num_bc_episodes 1000 \
         --no_render \
         --algo_sup \
-        dec14_gen_oracle_pick_place_data_1k
+        dec14_gen_oracle_reach2d_data_1k
--- scripts/run_sbatches.sh
+++ scripts/run_sbatches.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for experiment in /iliad/u/madeline/thriftydagger/scripts/thresholding_exps/extra-thresholds-eval/* 
+for experiment in /iliad/u/madeline/thriftydagger/scripts/gen_data/*.sh 
 do
     echo $experiment
     chmod u+x $experiment
-    sbatch --partition=iliad --time=7-00:00:00 --cpus-per-task=4 --gres=gpu:1 --mem=16G --nodes=1 --ntasks-per-node=1 --error=${experiment}_err.log --output=${experiment}_out.log $experiment
+    sbatch --partition=iliad --time=7-00:00:00 --cpus-per-task=4 --gres=gpu:titanxp:1 --mem=16G --nodes=1 --ntasks-per-node=1 --error=${experiment}_err.log --output=${experiment}_out.log $experiment
     sleep 1
 done
 # done
