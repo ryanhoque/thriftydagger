4b501447eef77166b1c02f690fa5a0151c8c90f6
Madeline Liao <madeline.m.liao@gmail.com>
2021-12-28 00:58:14 -0800

    add eval scripts

---
+++ scripts/dec27_vary_N/eval/bc/ensemble_linear.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ARCH=LinearModel
+CHECKPOINT_FILE=model_4.pt
+DATA_SOURCES=(oracle pi_r oracle_pi_r_mix)
+ENVIRONMENT=Reach2D
+METHOD=BC
+NS=(50 100 200 300 400 500 750 1000)
+NUM_MODELS=5
+SEED=4
+
+if [ $NUM_MODELS -gt 1 ]
+then
+    EXP_NAME_ARCH=Ensemble$ARCH
+else
+    EXP_NAME_ARCH=$ARCH
+fi
+
+for N in "${NS[@]}"
+do
+    for DATA_SOURCE in "${DATA_SOURCES[@]}"
+    do
+        python src/main.py \
+            --N $N \
+            --eval_only \
+            --model_path ./out/$ENVIRONMENT/$METHOD/$EXP_NAME_ARCH/$DATA_SOURCE\_N$N\_seed$SEED/$CHECKPOINT_FILE \
+            --exp_name dec27/$ENVIRONMENT/$METHOD/$EXP_NAME_ARCH/eval/$DATA_SOURCE\_N$N\_seed$SEED \
+            --data_path ./data/$ENVIRONMENT/$DATA_SOURCE.pkl \
+            --environment $ENVIRONMENT \
+            --method $METHOD \
+            --arch $ARCH \
+            --num_models $NUM_MODELS \
+            --seed $SEED
+    done
+done
